c0a55de55a61679ed3377c81350dcfb1625f0e3f
sschmocker <sandro.schmocker@itpoint.ch>
2021-12-17 12:42:43 +0100

    Update Company Portal & Win Installer

--- Sources/AppInstaller_Win32/AppInstaller_Source/Microsoft Desktop App Installer.ps1
+++ Sources/AppInstaller_Win32/AppInstaller_Source/Microsoft Desktop App Installer.ps1
@@ -1,3 +1,4 @@
 $Path = "$PSScriptRoot"
+Get-Childitem $Path -filter *.appxbundle | %{Add-AppxPackage -Path $_.FullName}
+Get-ChildItem $Path -Filter *.msixbundle | %{Add-AppxPackage -Path $_.FullName}
 Get-Childitem $Path -filter *.appx| %{Add-AppxPackage -Path $_.FullName}
-Get-ChildItem $Path -Filter *msixbundle | %{Add-AppxPackage -Path $_.FullName}
